fb2bd63d44004aae31e34d172ca397c3232d82f1
opilane <opilane@HP-CZC2349HT4>
2024-12-11 15:23:49 +0200

    11-12-24

--- _11_12_24.py
+++ _11_12_24.py
@@ -11,3 +11,18 @@ if A > 0:
         summa += i #summa = summma + i
         print(f"{i}. samm summa = {summa}")
     print(f"Vastus {summa}")
+######################
+
+##### Ülesanne 3 #####
+p = 1
+for j in range(8):
+    while True:
+        try:
+            arv = float(input(f"Sisesta {j+1} arv: "))
+            break
+        except:
+            print("On vaja reaalne arv")
+    if arv > 0: p *= arv
+    print(f"{j+1}. samm korrutis = {p}")
+print(f"Lõpputulemus on {p}")
+######################
