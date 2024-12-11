e6043fc44706ccad4e27c579bc0c4a4b905eed39
opilane <opilane@HP-CZC2349HT4>
2024-12-11 15:11:34 +0200

    11-12-24

--- _11_12_24.py
+++ _11_12_24.py
@@ -1,1 +1,13 @@
-
+##### Ülesanne 2 #####
+while True:
+    try:
+        A = int(input("Sisesta A: "))
+        break
+    except:
+        print("On vaja naturaalne arv")
+summa = 0
+if A > 0:
+    for i in range(1, A+1, 1):
+        summa += i #summa = summma + i
+        print(f"{i}. samm summa = {summa}")
+    print(f"Vastus {summa}")
